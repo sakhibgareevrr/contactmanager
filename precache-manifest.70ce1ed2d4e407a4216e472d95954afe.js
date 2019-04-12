f1f6a51c2cc1678e4a8dded0bb0ede4466c02953
Rushan Sakhibgareev <sakhibgareevrr@gmail.com>
2019-04-12 09:26:05 -0500

    Updates

---
+++ precache-manifest.70ce1ed2d4e407a4216e472d95954afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.4bdee9f6.js"
+  },
+  {
+    "revision": "3dd5dd13fb04207f688c",
+    "url": "/contactmanager/static/js/main.1d3618f0.chunk.js"
+  },
+  {
+    "revision": "1454300a490aec5a99af",
+    "url": "/contactmanager/static/js/2.a5819a3b.chunk.js"
+  },
+  {
+    "revision": "3dd5dd13fb04207f688c",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1454300a490aec5a99af",
+    "url": "/contactmanager/static/css/2.e049b07a.chunk.css"
+  },
+  {
+    "revision": "5c64e0e9221b85ef3f49e99a243a4071",
+    "url": "/contactmanager/index.html"
+  }
+];
